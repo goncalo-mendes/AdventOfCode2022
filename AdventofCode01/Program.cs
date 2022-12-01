004df401b60fe95359deee7ad89a6be0e12793a4
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-12-01 19:05:37 +0000

    Resolution to the first problem in C#

---
+++ AdventofCode01/Program.cs
@@ -0,0 +1,21 @@
+﻿        string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Gonçalo\RiderProjects\AdventofCode01\AdventofCode01\input.txt");
+        List<long> elves= new();
+        long current = 0;
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                elves.Add(current);
+                current = 0;
+            }
+            else
+            {
+                var calories = long.Parse(line);
+                current += calories;
+            }
+        }
+        elves.Add(current);
+        var result = elves.OrderByDescending(i => i).Take(1).Max();
+
+        Console.WriteLine(result);
+        System.Console.ReadKey();
