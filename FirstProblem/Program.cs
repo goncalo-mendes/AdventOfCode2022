53814e9822870fe0865bed907c3b04ced8fa660e
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-12-02 13:55:36 +0000

    Resolution to the second day

---
+++ FirstProblem/Program.cs
@@ -0,0 +1,48 @@
+﻿string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Gonçalo\RiderProjects\AdventofCode01\FirstProblem\input.txt");
+
+int score = 0;
+
+foreach (string round in lines)
+{
+    string opponentPlay = round.ElementAt(0).ToString();
+    string ownPlay = round.ElementAt(2).ToString();
+
+    if (ownPlay == "X")
+    {
+        score += 1;
+        if (opponentPlay == "A")
+        {
+            score += 3;
+        }
+        else if (opponentPlay == "C")
+        {
+            score += 6;
+        }
+    }
+    else if (ownPlay == "Y")
+    {
+        score += 2;
+        if (opponentPlay == "A")
+        {
+            score += 6;
+        }
+        else if (opponentPlay == "B")
+        {
+            score += 3;
+        }
+    }
+    else if (ownPlay == "Z")
+    {
+        score += 3;
+        if (opponentPlay == "B")
+        {
+            score += 6;
+        }
+        else if (opponentPlay == "C")
+        {
+            score += 3;
+        }
+    }
+}
+
+Console.WriteLine($"Your Rock Paper Scissors score is {score}.\n");
---
+++ SecondProblem/Program.cs
@@ -0,0 +1,59 @@
+﻿string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Gonçalo\RiderProjects\AdventofCode01\FirstProblem\input.txt");
+
+int score = 0;
+
+foreach (string round in lines)
+{
+    string opponentPlay = round.ElementAt(0).ToString();
+    string loseDrawWin = round.ElementAt(2).ToString();
+
+    if (loseDrawWin == "X")
+    {
+        if (opponentPlay == "A")
+        {
+            score += 3;
+        }
+        else if (opponentPlay == "B")
+        {
+            score += 1;
+        }
+        else if (opponentPlay == "C")
+        {
+            score += 2;
+        }
+    }
+    else if (loseDrawWin == "Y")
+    {
+        score += 3;
+        if (opponentPlay == "A")
+        {
+            score += 1;
+        }
+        else if (opponentPlay == "B")
+        {
+            score += 2;
+        }
+        else if (opponentPlay == "C")
+        {
+            score += 3;
+        }
+    }
+    else if (loseDrawWin == "Z")
+    {
+        score += 6;
+        if (opponentPlay == "A")
+        {
+            score += 2;
+        }
+        else if (opponentPlay == "B")
+        {
+            score += 3;
+        }
+        else if (opponentPlay == "C")
+        {
+            score += 1;
+        }
+    }
+}
+
+Console.WriteLine($"Your Rock Paper Scissors score is {score}.\n");
